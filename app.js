1544dfee4e891378a1310b6ea6de58f7ee94839d
王瑞瑞 <ruirui.wang@fengjr.comm>
2020-06-05 15:54:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,65 @@
+const exec = require('child_process').execSync
+const fs = require('fs')
+const superagent = require('superagent')
+
+// 公共变量
+const KEY = process.env.JD_COOKIE
+const serverJ = process.env.PUSH_KEY
+
+async function downFile () {
+    await superagent.get('https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js').end(async (err,res)=>{
+        if (err) {
+            console.log(err)
+            return
+        }
+        await fs.writeFileSync("./JD_DailyBonus.js",res.body, "binary")
+    })
+}
+
+async function changeFiele () {
+   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
+   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+}
+
+async function sendNotify (text,desp) {
+    try {
+        superagent
+            .post(`https://sc.ftqq.com/${serverJ}.send`)
+            .send({ text, desp })
+            .set('accept', 'json')
+            .end((err, res) => {
+                console.log(err)
+            });
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+async function start() {
+  if (!KEY) {
+    console.log('请填写 key 后在继续')
+    return
+  }
+  // 下载最新代码
+  await downFile();
+  console.log('下载代码完毕')
+  // 替换变量
+  await changeFiele();
+  console.log('替换变量完毕')
+  // 执行
+  await exec("node JD_DailyBonus.js >> result.txt");
+  console.log('执行完毕')
+
+  if (serverJ) {
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+      content = fs.readFileSync(path, "utf8");
+    }
+    await sendNotify("京东签到-" + new Date().toLocaleDateString(), content);
+    console.log('发送结果完毕')
+  }
+}
+
+start()
