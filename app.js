43d129dd1804c76d77d26adc7203922acd38a8d3
王瑞瑞 <ruirui.wang@fengjr.comm>
2020-06-05 16:38:31 +0800

    update

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// version v0.0.1
+// create by ruicky
+// detail url: https://github.com/ruicky/jd_sign_bot
+
 const exec = require('child_process').execSync
 const fs = require('fs')
 const superagent = require('superagent')
