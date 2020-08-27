acd71d7ffff0a751937e4abee76dce5e9bf261e6
lxk0301 <778732665@qq.com>
2020-08-27 19:08:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function downFile () {
 async function changeFiele () {
    let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
    content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
-   content = content.replace(/var DualKey = ''/, `var DualKey = '${KEY2}'`)    
+   //content = content.replace(/var DualKey = ''/, `var DualKey = '${KEY2}'`)    
    await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
 }
 
