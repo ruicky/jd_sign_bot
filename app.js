4b67b890ae223add9b5e21307f2a121d5d55bab6
chesterchenn <chesterchenn@outlook.com>
2021-01-21 20:01:56 +0800

    静态语法的修复

--- app.js
+++ app.js
@@ -12,65 +12,63 @@ const KEY = process.env.JD_COOKIE;
 const serverJ = process.env.PUSH_KEY;
 const DualKey = process.env.JD_COOKIE_2;
 
-
-async function downFile () {
-    // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
-    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
-    await download(url, './');
+async function downFile() {
+  // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
+  const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
+  await download(url, './');
 }
 
-async function changeFile () {
-   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
-   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
-   if (DualKey) {
+async function changeFile() {
+  let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8');
+  content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
+  if (DualKey) {
     content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
-   }
-   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
+  }
+  await fs.writeFileSync('./JD_DailyBonus.js', content, 'utf8');
 }
 
-async function sendNotify (text,desp) {
-  const options ={
-    uri:  `https://sc.ftqq.com/${serverJ}.send`,
+async function sendNotify(text, desp) {
+  const options = {
+    uri: `https://sc.ftqq.com/${serverJ}.send`,
     form: { text, desp },
     json: true,
-    method: 'POST'
-  }
-  await rp.post(options).then(res=>{
-    console.log(res)
-  }).catch((err)=>{
-    console.log(err)
-  })
+    method: 'POST',
+  };
+  await rp.post(options).then((res) => {
+    console.log(res);
+  }).catch((err) => {
+    console.log(err);
+  });
 }
 
 async function start() {
   if (!KEY) {
-    console.log('请填写 key 后在继续')
-    return
+    console.log('请填写 key 后在继续');
+    return;
   }
   // 下载最新代码
   await downFile();
-  console.log('下载代码完毕')
+  console.log('下载代码完毕');
   // 替换变量
   await changeFile();
-  console.log('替换变量完毕')
+  console.log('替换变量完毕');
   // 执行
-  await exec("node JD_DailyBonus.js >> result.txt");
-  console.log('执行完毕')
+  await exec('node JD_DailyBonus.js >> result.txt');
+  console.log('执行完毕');
 
   if (serverJ) {
-    const path = "./result.txt";
-    let content = "";
+    const path = './result.txt';
+    let content = '';
     if (fs.existsSync(path)) {
-      content = fs.readFileSync(path, "utf8");
+      content = fs.readFileSync(path, 'utf8');
     }
-    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
-    let res = t ? t[1].replace(/\n/,'') : '失败'
-    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
-    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+    const t = content.match(/【签到概览】:((.|\n)*)【签到总计】/);
+    const res = t ? t[1].replace(/\n/, '') : '失败';
+    const t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/);
+    const res2 = t2 ? t2[1].replace(/\n/, '') : '总计0';
 
-    
     await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
   }
 }
 
-start()
+start();
